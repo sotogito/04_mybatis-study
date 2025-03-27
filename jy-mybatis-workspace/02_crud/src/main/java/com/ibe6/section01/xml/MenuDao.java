24fe54da944e81383bf9f24f3595a25d2f22a1d3
ibe6 <nxnxnxnx626@gmail.com>
2025-03-27 17:27:44 +0900

    0327

--- jy-mybatis-workspace/02_crud/src/main/java/com/ibe6/section01/xml/MenuDao.java
+++ jy-mybatis-workspace/02_crud/src/main/java/com/ibe6/section01/xml/MenuDao.java
@@ -3,7 +3,23 @@ package com.ibe6.section01.xml;
 import com.ibe6.dto.MenuDto;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.List;
+
 public class MenuDao {
+
+
+        /*
+         * 기존의 JDBC방식
+        public 결과 메소드(Connection conn, 데이터){
+            1. PreaparedStatement 생성
+            2. 파라미터 바인딩 (? 에 데이터 채우기)
+            3. sql 실행
+            4. 결과 매핑 (rset 데이터꺼내서 dto의 필드에 담기)
+            5. 자원반납
+            6. 결과반환
+        }
+     */
+
 
     /*
         ## MyBatis 방식으로 xml파일에 작성된 sql문 실행 ##
@@ -22,9 +38,32 @@ public class MenuDao {
             무조건 한 개만 전달 가능 (AnyType)
             즉, 다수의 데이터일 경우 Map 또는 DTO에 담아서 전달
             전달된 데이터가 xml의 sql문에 알아서 바인딩됨 (물론 xml에서 바인딩 처리해야됨)
-
      */
+
     public int insertMenu(SqlSession sqlSession, MenuDto menu){
         return sqlSession.insert("MenuMapper.insertMenu", menu);
+    }
+
+    public int updateMenu(SqlSession sqlSession, MenuDto menu){
+        return sqlSession.update("MenuMapper.updateMenu", menu);
+    }
+
+    public int deleteMenu(SqlSession sqlSession, int code){
+        return sqlSession.delete("MenuMapper.deleteMenu", code);
+    }
+
+    public List<String> selectMenuNameList(SqlSession sqlSession){
+        return sqlSession.selectList("MenuMapper.selectMenuNameList");
+    }
+
+    public List<MenuDto> selectOrderableMenuList(SqlSession sqlSession){
+        return sqlSession.selectList("MenuMapper.selectOrderableMenuList");
+    }
+    public int selectOrderableMenuCount(SqlSession sqlSession){
+        return sqlSession.selectOne("MenuMapper.selectOrderableMenuCount");
+    }
+
+    public MenuDto selectMenuByCode(SqlSession sqlSession, int code){
+        return sqlSession.selectOne("MenuMapper.selectMenuByCode", code);
     }
 }
