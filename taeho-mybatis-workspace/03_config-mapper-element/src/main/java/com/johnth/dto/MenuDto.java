e93abacdf68a550416e2e91fc799828f47b797f5
TH <ok821000@gmail.com>
2025-03-28 18:59:40 +0900

    [Add] Mybatis를 활용한 MVC 심화 학습진행

---
+++ taeho-mybatis-workspace/03_config-mapper-element/src/main/java/com/johnth/dto/MenuDto.java
@@ -0,0 +1,23 @@
+package com.johnth.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class MenuDto {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String orderableStatus;
+
+    // 해당 메뉴가 가지는 카테고리 정보를 담기위한 DTO
+    // 1:1 (has-a) 포함관계
+    private CategoryDto category;
+
+}
