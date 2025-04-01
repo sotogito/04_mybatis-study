eec38236bd74a7232eb6063c5ab7d3e0e51b5ba5
sotogito <sotogitoarchive@gmail.com>
2025-04-01 11:03:10 +0900

    Initial commit

---
+++ sooyeon-mybatis-workspace/05_junit/src/main/java/com/sotogito/section01/tests/Calculator.java
@@ -0,0 +1,12 @@
+package com.sotogito.section01.tests;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int random() {
+        return 0;
+    }
+}
---
+++ sooyeon-mybatis-workspace/05_junit/src/main/java/com/sotogito/section02/junit/Calculator.java
@@ -0,0 +1,13 @@
+package com.sotogito.section02.junit;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int random() {
+        return (int)(Math.random() * 101+100);
+    }
+
+}
