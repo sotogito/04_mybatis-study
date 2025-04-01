393df5a1703d039a41845bef5b2e3f667141721d
Seoyeon <106144694+podoseee@users.noreply.github.com>
2025-04-01 16:41:29 +0900

    create 05_junit

---
+++ seoyeon-mybatis-workspace/05_junit/src/main/java/com/podoseee/section02/junit/Calculator.java
@@ -0,0 +1,13 @@
+package com.podoseee.section02.junit;
+
+public class Calculator {
+
+    public int sum(int x, int y){
+        return x + y;
+    }
+
+    public int random(){
+        return (int)(Math.random() * 101 + 100);
+    }
+
+}
---
+++ seoyeon-mybatis-workspace/05_junit/src/main/java/com/podoseee/section03/dto/MenuDto.java
@@ -0,0 +1,20 @@
+package com.podoseee.section03.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+
+public class MenuDto {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String orderableStatus;
+
+}
